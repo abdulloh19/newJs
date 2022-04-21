2d188395240bc90bc7ae40791ba7717559d61ee5
abdulloh19 <parij4320@gmail.com>
2022-04-21 17:15:29 +0500

    frist js

---
+++ main.js
@@ -0,0 +1,14 @@
+const a = prompt("birinchi sonni kiriting");
+const b = prompt("ikkinchi sonni kiriting");
+
+
+const yigindi = Number(a) + Number(b);
+const ayirma = a - b;
+const kopaytma = a * b;
+const bolinma = a / b;
+
+alert(`yig'ndi: ${yigindi};
+ayirma: ${ayirma}; 
+kopaytma: ${kopaytma}; 
+bolinma: ${bolinma}`)   
+  
