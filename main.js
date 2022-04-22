5a2d41e9581aac04ab21a9c8f76711079381b3d7
abdulloh19 <parij4320@gmail.com>
2022-04-22 18:04:05 +0500

    mantiqiy opertorlar

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-const a = prompt("birinchi sonni kiriting");
-const b = prompt("ikkinchi sonni kiriting");
+// const a = prompt("birinchi sonni kiriting");
+// const b = prompt("ikkinchi sonni kiriting");
 
 
-const yigindi = Number(a) + Number(b);
-const ayirma = a - b;
-const kopaytma = a * b;
-const bolinma = a / b;
+// const yigindi = Number(a) + Number(b);
+// const ayirma = a - b;
+// const kopaytma = a * b;
+// const bolinma = a / b;
 
-alert(`yig'ndi: ${yigindi};
-ayirma: ${ayirma}; 
-kopaytma: ${kopaytma}; 
-bolinma: ${bolinma}`)   
+// alert(`yig'ndi: ${yigindi};
+// ayirma: ${ayirma}; 
+// kopaytma: ${kopaytma}; 
+// bolinma: ${bolinma}`)   
   
+const a = prompt("Birinchi sonni kiriting");
+const b = prompt("Ikkinchi sonni kiriting");
+const v = prompt("Siz o'ylagan yig'indini kiriting");
+
+const sum = +a + +b;
+
+const resultWrapper = document.querySelector(".rasult")
+
+if (sum == +v) {
+    resultWrapper.textContent = "Siz to'g'ri toptingiz"
+} else if (sum < v) {
+    resultWrapper.textContent = "Siz kotta qiymat kiritingiz"
+} else if (sum > v) {
+    resultWrapper.textContent = "Siz kichkina qiymat kiritingiz"
+} else {
+    resultWrapper.textContent = "Siz noto'g'ri qiymat kiritingiz"
+}
+
